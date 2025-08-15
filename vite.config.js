0f7e3629f2244b918ef1b9b4af27697233411d02
Chris Garciae <124018793+xxlusubuxx@users.noreply.github.com>
2025-08-15 14:16:58 +0700

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If your repo is https://github.com/<user>/<repo-name>
+  base: '/',        // <-- set this
+  // If your repo is <user>.github.io (root domain), use: base: '/'
 })
